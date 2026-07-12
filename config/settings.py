00bf2d791cd2c7fa9ac9230f13a4f714c75d4ac4
Mohsen <sm.mirhafez86@gmail.com>
2026-07-12 12:34:19 +0330

    get static and media root from .env

--- config/settings.py
+++ config/settings.py
@@ -127,14 +127,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = os.getenv('STATIC_URL', 'static/')
+STATIC_ROOT = os.getenv('STATIC_ROOT', BASE_DIR / 'static')
 
 # Media files (User uploads)
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = os.getenv('MEDIA_URL', 'media/')
+MEDIA_ROOT = os.getenv('MEDIA_ROOT', BASE_DIR / 'media')
 
 
 # Default primary key field type
